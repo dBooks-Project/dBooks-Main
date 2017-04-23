20564b11aa5eaf0402d204d546fb8e11c933e2b7
JulienFerluc <ferluc.julien@carrefour.cegepvicto.ca>
2017-04-23 15:00:29 -0400

    Finished register page

--- routes/connection.js
+++ routes/connection.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/connection', function(req, res, next) {
-    res.render('connection');
-});
+router
+    .get('/connection', function(req, res, next) {
+
+        res.render('connection');
+    })
+
+    .post('/connection', function(req, res, next) {
+
+        res.end();
+    });
 
 module.exports = router;
