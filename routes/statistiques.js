071f2e129fd6c2696b378f4944927e861d761da4
dlafm <laflamme-morin.daphne@carrefour.cegepvicto.ca>
2017-05-01 20:31:44 -0400

    made the stats kinda pretty

--- routes/statistiques.js
+++ routes/statistiques.js
@@ -7,10 +7,13 @@ var query1 = "SELECT COUNT(*) AS NombreDeLivres FROM Livres;";
 var query2 = "SELECT COUNT(LangueID) AS LangueFrancaise FROM Livres WHERE LangueID=1;";
 var query3 = "SELECT COUNT(LangueID) AS LangueAnglaise FROM Livres WHERE LangueID=2;";
 var query4 = "SELECT COUNT(LangueID) AS LangueEspagnole FROM Livres WHERE LangueID=3;";
+var query5 = "SELECT COUNT(LangueID) AS LangueJaponaise FROM Livres WHERE LangueID=4;";
+var query6 = "SELECT COUNT(LangueID) AS LangueBresilienne FROM Livres WHERE LangueID=5;";
+var query7 = "SELECT COUNT(LangueID) AS LangueItalienne FROM Livres WHERE LangueID=6;";
 
 /* GET home page. */
 router.get('/statistiques', function(req, res, next) {
-    connection.query(query1 + query2 + query3 + query4,(err, results, fields) => {
+    connection.query(query1 + query2 + query3 + query4 + query5 + query6 + query7,(err, results, fields) => {
         if(err) {
             console.log(err);
             
@@ -22,7 +25,10 @@ router.get('/statistiques', function(req, res, next) {
                 nombre: results[0][0],
                 langueFrancaise: results[1][0],
                 langueAnglaise: results[2][0],
-                langueEspagnole: results[3][0]
+                langueEspagnole: results[3][0],
+                langueJaponaise: results[4][0],
+                langueBresilienne: results[5][0],
+                langueItalienne: results[6][0]
             }
         });
     });
