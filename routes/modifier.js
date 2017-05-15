02e61e95dd71e9ae1fc6fbc4df16369adcc93dbe
Entrybot <ferluc.julien@carrefour.cegepvicto.ca>
2017-05-14 22:22:39 -0400

    Update Books

--- routes/modifier.js
+++ routes/modifier.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var query = require('../middleware/query')
+var connection = require('../middleware/connect')
+var query = require("../middleware/query")
 
 var queryRead = "SELECT * FROM AuteursLivres INNER JOIN Livres ON LivreID = Livres.ID INNER JOIN Auteurs ON AuteurID = Auteurs.ID INNER JOIN GenresLitteraires ON GenresLitteraires.ID = GenreID INNER JOIN Langues ON Langues.ID = LangueID Where Livres.ID = ?"
+var queryUpdateLivres = "UPDATE Livres SET Titre = ?, AnneeParution = ?, LangueID = ?, GenreID = ? WHERE ID = ?;";
+var queryUpdateAuteurs = "UPDATE Auteurs SET Nom = ?, Prenom = ? WHERE ID = ?";
 
 router
     .get('/modifier/:id', function (req, res, next) {
@@ -15,9 +18,20 @@ router
         });
     })
     .post('/modifier/:id', (req, res) => {
-        query.simple("UPDATE Livres SET Titre = ?")
-        res.redirect('/bibliotheque');
+        query.complex("SELECT ID FROM Langues WHERE NomLangue = ?", [req.body.langue], (langueID) => {
+            query.complex("SELECT ID FROM GenresLitteraires WHERE NomGenre = ?", [req.body.genre], (genreID) => {
+                query.complex("SELECT Auteurs.ID FROM AuteursLivres INNER JOIN Auteurs ON Auteurs.ID = AuteurID INNER JOIN Livres ON LivreID = Livres.ID WHERE LivreID = ?", [req.params.id], (auteurID) => {
+                connection.query(queryUpdateLivres, [req.body.titre, req.body.annee,langueID[0].ID, genreID[0].ID ,req.params.id], (err,rows,fields) => {
+                    if(err) throw err;
+                    connection.query(queryUpdateAuteurs, [req.body.nom, req.body.prenom, auteurID[0].ID], (err, rows, fields) => {
+                        if(err) throw err;
+                        res.redirect('/bibliotheque');
+                    });
+                });
+            });
+        });
     });
+});
 
 
 
